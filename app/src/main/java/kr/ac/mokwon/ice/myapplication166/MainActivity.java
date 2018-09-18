c57f3119cf7a0fd6ebf1b76d6778d7b9def18749
sosobal <sosobal1024@naver.com>
2018-09-18 16:30:34 +0900

    [2018-09-13] Calculator commit

--- app/src/main/java/kr/ac/mokwon/ice/myapplication166/MainActivity.java
+++ app/src/main/java/kr/ac/mokwon/ice/myapplication166/MainActivity.java
@@ -5,6 +5,24 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected double ans= 0.; //Answer
+
+    protected void inputNum() {
+
+    }
+
+    double op4(double x) {
+        return  0.;
+    }
+
+    double mathOp(double x) {
+        return  0.;
+    }
+
+    void printNum(double x) {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
